5d6d98de3ba3e6e41dc5c491470d658a7a1ed82c
Vodoleev <78915707+Vodoleev@users.noreply.github.com>
2021-05-03 18:14:13 +0300

    Update CipherAlg.py

--- Desktop/CipherAlg.py
+++ Desktop/CipherAlg.py
@@ -1,13 +1,26 @@
 import sys
+import requests
+from urllib.parse import urlparse
 
 
+def file_name(path):
+    prename = urlparse(path).path
+    return prename[prename.rfind('/') + 1::]
+def isURL(path):
+    return (urlparse(path).scheme != '' and urlparse(path).netloc != '')
+def download(path):
+    return bytearray(requests.get(path).content)
 def read(path):
-    with open(path, 'rb') as file:
-        return bytearray(file.read())
+    if isURL(path):
+        return bytearray(download(path))
+    else:
+        with open(path, 'rb') as file:
+            return bytearray(file.read())
 def write(path, data):
+    if isURL(path):
+    	path = file_name(path)
     with open(path, 'wb') as file:
         file.write(bytes(data))
-        
 def CaesarEncryption(data, key):
     for i in range(len(data)):
         data[i] = (data[i] + key) % 256
@@ -36,4 +49,3 @@ def VernamDecryption(data, keyWord):
     for i in range(len(data)):
         data[i] = (data[i] ^ keyWord[i % len(keyWord)]) % 256
     return data
-
